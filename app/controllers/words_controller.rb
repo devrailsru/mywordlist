0bb166a562078ddb1cfe1940ecd3541beeea5750
PinkDeer <your.email@example.com>
2018-04-11 12:52:48 +0300

    Update words controller

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,7 +1,52 @@
 class WordsController < ApplicationController
 
+  before_action :find_word, only: [:show, :edit, :update, :destroy]
+
   def index
     @words = Word.all
+  end
+
+  def show
+  end
+
+  def new
+    @word = Word.new
+  end
+
+  def create
+    @word = Word.new(word_params)
+    if @word.save
+     redirect_to @word
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @word = Word.find(params[:id])
+  end
+
+  def update
+    if @word.update_attributes(word_params)
+      redirect_to @word
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @word.destroy
+    redirect_to words_path
+  end
+
+  private
+
+  def find_word
+    @word = Word.find(params[:id])
+  end
+
+  def word_params
+    params.require(:word).permit(:title, :translation, :example)
   end
 
 end
