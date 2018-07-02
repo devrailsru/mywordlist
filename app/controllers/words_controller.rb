b5a16685869f6179e5108cfee1357613bf5c5244
PinkDeer <bunman@mail.ru>
2018-07-02 14:20:31 +0300

    Edited index action: displaying only one's own words

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -4,31 +4,32 @@ class WordsController < ApplicationController
   before_action :find_word, only: [:show, :edit, :update, :destroy]
 
   def index
-    @words, @alphaParams = Word.all.alpha_paginate(params[:letter]){|word| word.title}
+    @words, @alphaParams = current_user.words.all.alpha_paginate(params[:letter]){|word| word.title}
+    @word = current_user.words.build
   end
 
   def show
   end
 
   def new
-    @word = Word.new
+    @word = current_user.words.build
   end
 
   def create
-    @word = Word.new(word_params)
+    @word = current_user.words.build(word_params)
     if @word.save
      redirect_to @word
     else
       render :new
     end
+
   end
 
   def edit
-    @word = Word.find(params[:id])
   end
 
   def update
-    if @word.update_attributes(word_params)
+    if @word.update(word_params)
       redirect_to @word
     else
       render :edit
