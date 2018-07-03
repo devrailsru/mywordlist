646a1f6e80277851c6ca436b4e23d2ec4aed3bef
PinkDeer <bunman@mail.ru>
2018-07-03 12:10:29 +0300

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
 
   # For Devise gem
   config.action_mailer.default_url_options = { host: 'mywordlistru.herokuapp.com'}
-  config.action_mailer.delivery_method = :smpt
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
