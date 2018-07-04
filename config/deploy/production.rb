132ae6b58de0ef11b4fc42da60453986b21ee1aa
PinkDeer <bunman@mail.ru>
2018-07-04 22:24:01 +0300

    fix

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '185.221.153.241/', user: 'deploy', roles: %w{app db web}
+server '185.221.153.241', user: 'deploy', roles: %w{app db web}
 
 # role-based syntax
 # ==================
