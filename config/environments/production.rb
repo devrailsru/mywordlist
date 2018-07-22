b2b285f93287a45474c74f9774a380b62fdf450d
PinkDeer <bunman@mail.ru>
2018-07-22 15:41:05 +0300

    Update settings for mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,6 +85,7 @@ Rails.application.configure do
 
   # For Devise gem
   config.action_mailer.default_url_options = { host: 'mywordlist.ru'}
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   tls: true,
